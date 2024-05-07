4d5463add266ce505a03a449af219e275d05c33a
Olajumoke-1 <Olajumokesolihat311@gmail.com>
2024-05-07 15:51:45 +0100

    Implemented monkey emotions toggling

--- Week 6/Day 1/script.js
+++ Week 6/Day 1/script.js
@@ -1,4 +1,14 @@
 const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
 
-// Add event listener here
+// // Add event listener here
+closedFace.addEventListener('click', () => {
+    closedFace. classList.add('hidden');
+    openFace. classList.remove('hidden');
+});
+
+// Event listener for openFace element
+openFace.addEventListener('click', () => {
+    openFace. classList.add('hidden');
+    closedFace. classList.remove('hidden');
+});
